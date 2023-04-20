140a6dcbc774c3e1aa7f8013d99c33e7898b43f7
Alfeu U. Tavares <autavares.dev@gmail.com>
2023-04-20 18:27:44 -0300

    Add automatic column resizing after starting

--- src/procman/ProcessList.java
+++ src/procman/ProcessList.java
@@ -11,13 +11,15 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.RowSorter;
+import javax.swing.SortOrder;
 import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("serial")
 public final class ProcessList extends JPanel {
-	private JTable processesTable;
+	private JTable table;
 	private JScrollPane scrollPane;
 	private Timer updateTimer;
 
@@ -64,24 +66,28 @@ public final class ProcessList extends JPanel {
 		// Creates JTable passing a TableModel to allow data updating and
 		// disables 'setAutoCreateColumnsFromModel' to keep columns sizes after
 		// updating the data.
-		// TODO: add automatic column resizing on creation.
 		var model = new DefaultTableModel(COLUMN_NAMES, 0);
-		processesTable = new JTable(model);
-		processesTable.setDefaultEditor(Object.class, null);
-		processesTable.setAutoCreateColumnsFromModel(false);
-		processesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		processesTable.setAutoCreateRowSorter(true);
-		processesTable.setColumnSelectionAllowed(false);
+		table = new JTable(model);
+		table.setDefaultEditor(Object.class, null);
+		table.setAutoCreateColumnsFromModel(false);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setAutoCreateRowSorter(true);
+		table.setColumnSelectionAllowed(false);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
 		// Initializes empty table inside scroll table, not editable.
 		scrollPane = new JScrollPane();
 		add(scrollPane, BorderLayout.CENTER);
-		scrollPane.setViewportView(processesTable);
+		scrollPane.setViewportView(table);
+
+		// First update is invoked manually, resizes columns and sorts by PID.
+		updateTable();
+		resizeColumns();
+		table.getRowSorter().setSortKeys(
+				Arrays.asList(new RowSorter.SortKey(0, SortOrder.ASCENDING)));
 
 		// Updates the process table each 1 seconds (1000ms).
-		// First update is manually invoked.
-		updateTable();
-		updateTimer = new Timer(1000, (ev) -> {
+		updateTimer = new Timer(1000, (_ev) -> {
 			updateTable();
 		});
 		updateTimer.setRepeats(true);
@@ -122,15 +128,15 @@ public final class ProcessList extends JPanel {
 				.toArray(Object[][]::new);
 
 		// TODO: add right-click menu with actions to selected process.
-		var model = (DefaultTableModel) processesTable.getModel();
+		var model = (DefaultTableModel) table.getModel();
 
 		// Stores current sorting keys.
 		// TODO: could be done using a sorting change event in the table?
-		final var sortKeys = processesTable.getRowSorter().getSortKeys();
+		final var sortKeys = table.getRowSorter().getSortKeys();
 
 		// Stores current selected PID.
 		// TODO: could be done using a click event in the row?
-		final var selectedRow = processesTable.getSelectedRow();
+		final var selectedRow = table.getSelectedRow();
 		final var selectedPid = selectedRow != -1
 				? model.getValueAt(selectedRow, 0).toString()
 				: null;
@@ -140,15 +146,42 @@ public final class ProcessList extends JPanel {
 		model.fireTableDataChanged();
 
 		// Sorts again with same keys.
-		processesTable.getRowSorter().setSortKeys(sortKeys);
+		table.getRowSorter().setSortKeys(sortKeys);
 
 		// Reselects row of the selected PID if any.
 		if (selectedPid != null) {
 			for (var i = 0; i < model.getRowCount(); i++) {
 				if (model.getValueAt(i, 0).toString().equals(selectedPid)) {
-					processesTable.setRowSelectionInterval(i, i);
+					table.setRowSelectionInterval(i, i);
+					break;
 				}
 			}
+		}
+	}
+
+	/**
+	 * Resizes each columns to have 2x the width of the largest cell, based
+	 * on the current table data.
+	 */
+	public void resizeColumns() {
+		var columnModel = table.getColumnModel();
+		for (var j = 0; j < COLUMN_NAMES.length; j++) {
+			var col = columnModel.getColumn(j);
+			var renderer = col.getHeaderRenderer();
+			var width = 0;
+
+			for (var i = 0; i < table.getRowCount(); i++) {
+				renderer = table.getCellRenderer(i, j);
+				var comp = renderer.getTableCellRendererComponent(
+						table,
+						table.getValueAt(i, j),
+						false,
+						false,
+						i, j);
+				width = Math.max(width, comp.getPreferredSize().width);
+			}
+
+			col.setPreferredWidth(width * 2);
 		}
 	}
 }
