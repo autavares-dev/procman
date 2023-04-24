0d2866ec12830ece7b6c720fa32053cdd5e4374b
Alfeu U. Tavares <autavares.dev@gmail.com>
2023-04-24 09:51:10 -0300

    Fix system info alignment

--- src/procman/SystemInfo.java
+++ src/procman/SystemInfo.java
@@ -1,55 +1,82 @@
 package procman;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
+
+class Section {
+	public record Content(String title, String content) {
+	};
+
+	public static JPanel Create(String title, Content[] contents) {
+		var section = new JPanel();
+		var grid = new GridLayout(1 + contents.length, 1);
+		grid.setVgap(2);
+		section.setLayout(grid);
+
+		section.add(createTitle(title));
+		for (var content : contents) {
+			section.add(createInfo(content.title, content.content));
+		}
+
+		return section;
+	}
+
+	private static Box createTitle(String title) {
+		return createRow(16, title, "");
+	}
+
+	private static Box createInfo(String title, String content) {
+		return createRow(14, title, content);
+	}
+
+	private static Box createRow(int titleSize, String title, String content) {
+		var row = Box.createHorizontalBox();
+
+		var titleLabel = new JLabel(title + ": ");
+		titleLabel
+				.setFont(titleLabel.getFont().deriveFont(Font.BOLD, titleSize));
+		var contentLabel = new JLabel(content);
+
+		row.add(titleLabel);
+		row.add(contentLabel);
+		row.add(Box.createGlue());
+
+		return row;
+	}
+}
 
 @SuppressWarnings("serial")
 public final class SystemInfo extends JPanel {
 	public SystemInfo() {
 		setLayout(new BorderLayout());
-
+		setBorder(new EmptyBorder(8, 8, 8, 8));
 		// Vertical stack on the top of the screen.
 		var content = new JPanel();
 		content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 		add(content, BorderLayout.NORTH);
 
-		// TODO: align labels to the left of the 'content' panel.
+		Section.Content[] osSection = {
+			new Section.Content("O.S.", System.getProperty("os.name")),
+			new Section.Content("Version", System.getProperty("os.version")),
+			new Section.Content("Arch.", System.getProperty("os.arch"))
+		};
+		content.add(Section.Create("Operational System", osSection));
 
-		content.add(createTitle("Operational System"));
-		content.add(createInfo("O.S.", System.getProperty("os.name")));
-		content.add(createInfo("Version", System.getProperty("os.version")));
-		content.add(createInfo("Arch.", System.getProperty("os.arch")));
+		content.add(new JSeparator());
 
-		content.add(createTitle("Java"));
-		content.add(createInfo("Version", System.getProperty("java.version")));
-		content.add(createInfo("Vendor", System.getProperty("java.vendor")));
-		content.add(createInfo("URL", System.getProperty("java.vendor.url")));
-	}
-
-	static JPanel createTitle(String title) {
-		return createRow(16, title, "");
-	}
-
-	static JPanel createInfo(String title, String content) {
-		return createRow(14, title, content);
-	}
-
-	static JPanel createRow(int titleSize, String title, String content) {
-		var row = new JPanel();
-		row.setLayout(new FlowLayout());
-
-		var titleLabel = new JLabel(title + ": ");
-		titleLabel.setFont(row.getFont().deriveFont(Font.BOLD, titleSize));
-		var contentLabel = new JLabel(content);
-
-		row.add(titleLabel);
-		row.add(contentLabel);
-
-		return row;
+		Section.Content[] javaSection = {
+			new Section.Content("Version", System.getProperty("java.version")),
+			new Section.Content("Vendor", System.getProperty("java.vendor")),
+			new Section.Content("URL", System.getProperty("java.vendor.url"))
+		};
+		content.add(Section.Create("Java", javaSection));
 	}
 }
